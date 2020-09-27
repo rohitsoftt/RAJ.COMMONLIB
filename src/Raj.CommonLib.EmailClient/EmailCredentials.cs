4aa944537e2f5ce5bfc65682e06952f37359b8f6
Rohit Jadhav <rohit.a.jadhav95@gmail.com>
2020-09-27 15:00:35 +0530

    email client init

---
+++ src/Raj.CommonLib.EmailClient/EmailCredentials.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raj.CommonLib.EmailClient
+{
+    public class EmailCredentials
+    {
+        /// <summary>
+        /// Username of the email
+        /// </summary>
+        public string Username { get; set; }
+        /// <summary>
+        /// Password of the email
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// store in comma seprated format
+        /// </summary>
+        public string CCEmails { get; set; }
+        /// <summary>
+        /// To meail
+        /// </summary>
+        public string ToEmails { get; set; }
+        /// <summary>
+        /// SMTP of the email server
+        /// </summary>
+        public string SMTP { get; set; }
+        /// <summary>
+        /// Port of the SMTP server
+        /// </summary>
+        public string Port { get; set; }
+        /// <summary>
+        /// From name to send email
+        /// </summary>
+        public string FromName { get; set; }
+        /// <summary>
+        /// From email address 
+        /// </summary>
+        public string FromEmailAddress { get; set; }
+        /// <summary>
+        /// Created date
+        /// </summary>
+        public string CreatedDate { get; set; }
+        /// <summary>
+        /// Reply to emails comma(",") seprated list
+        /// </summary>
+        public string ReplyToEmails { get; set; }
+        /// <summary>
+        /// Get email credentails
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static EmailCredentials GetCredentials()
+        {
+            return new EmailCredentials
+            {
+                Username = Properties.EmailResource.Username,
+                Password = Properties.EmailResource.Password,
+                CCEmails = Properties.EmailResource.CCEmailIds,
+                ToEmails = Properties.EmailResource.ToEmailIds,
+                ReplyToEmails = Properties.EmailResource.ReplyToEmails,
+                SMTP = Properties.EmailResource.SMTP,
+                Port = Properties.EmailResource.SMTPPort,
+                FromName = Properties.EmailResource.FromEmailDisplayName,
+                FromEmailAddress = Properties.EmailResource.FromEmailId
+            };
+        }
+    }
+}
---
+++ src/Raj.CommonLib.EmailClient/EmailService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raj.CommonLib.EmailClient
+{
+    class EmailService
+    {
+        /// <summary>
+        /// Use to send any type of email
+        /// </summary>
+        /// <returns></returns>
+        private static async Task<bool> SendEmail(EmailCredentials emailCredentials, string subject, string body, string[] attachmentPath = null)
+        {
+            var credentials = new NetworkCredential(emailCredentials.Username, emailCredentials.Password);
+            // Mail message
+            var mail = new MailMessage()
+            {
+                From = new MailAddress(emailCredentials.FromEmailAddress, emailCredentials.FromName),
+                Subject = subject,
+                Body = body
+            };
+            mail.IsBodyHtml = true;
+            mail.To.Add(emailCredentials.ToEmails);
+            mail.ReplyToList.Add(emailCredentials.ReplyToEmails);
+            string[] CCEamailIDs = !string.IsNullOrEmpty(emailCredentials.CCEmails) ? emailCredentials.CCEmails.Split(',') : null;
+            foreach (string CCEmail in CCEamailIDs)
+            {
+                mail.CC.Add(new MailAddress(CCEmail)); //Adding Multiple CC email Id
+            }
+
+            // If not null the attachemnt
+            if (attachmentPath != null && attachmentPath.Length > 0)
+            {
+                foreach (var attachment in attachmentPath)
+                {
+                    var attachmentt = new System.Net.Mail.Attachment(attachment);
+                    mail.Attachments.Add(attachmentt);
+                }
+            }
+
+            // Smtp client
+            var client = new SmtpClient()
+            {
+                Port = Int32.Parse(emailCredentials.Port),
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Host = emailCredentials.SMTP,
+                EnableSsl = true,
+                Credentials = credentials
+            };
+            try
+            {
+                await client.SendMailAsync(mail);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
